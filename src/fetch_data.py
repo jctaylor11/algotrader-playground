ed70bb1efb50298f8de593f889764554442a8924
jctaylor11 <68626010+jctaylor11@users.noreply.github.com>
2026-01-12 18:39:52 +0900

    Refactor get_binance_ohlcv and add associated jupyter notebook

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -8,27 +8,24 @@ load_dotenv()
 api_key = os.getenv("API_KEY")
 api_secret = os.getenv("SECRET_KEY")
 
+# Initialise Binance client (API wrapper binance-python) 
 client = Client(api_key, api_secret, tld = "com")
 
-def get_history(symbol, interval, start, end=None): 
 
-    # Get historical market data from API client and save to dataframe
+def get_binance_ohlcv(symbol, interval, start, end=None):
+    """Fetch and clean historical OHLCV data from Binance using python-binance wrapper"""
     bars = client.get_historical_klines(symbol, interval, start, end)
     df = pd.DataFrame(bars)
+                      
+    df.columns = ["Open time", "Open", "High", "Low", "Close", "Volume", "Close time", "Quote asset volume", "Number of trades", "Taker buy base asset volume", "Taker buy quote asset volume", "Ignore"]
 
-    df["Date"] = pd.to_datetime(df.loc[:,0], unit="ms")
-    df.columns = ["Open Time", "Open", "High", "Low", "Close", "Volume",
-                  "Close Time", "Quote Asset Volume", "Number of Trades",
-                  "Taker Buy Base Asset Volume", "Taker Buy Quote Asset Volume", "Ignore", "Date"]
-    
-    # Filter the df to the columns I want and set the date as the index
-    df = df[["Date", "Open", "High", "Low", "Close", "Volume"]].copy()
-    df.set_index("Date", inplace=True)
+    df["Date"] = pd.to_datetime(df["Open time"], unit='ms')
+    df.set_index("Date", inplace = True)
 
-    # Pandas defaults to object type, so we convert columns to numeric, verified with df.dtypes
-    for column in df.columns:
-        df[column] = pd.to_numeric(df[column], errors="coerce")
+    df = df[["Open", "High", "Low", "Close", "Volume"]].copy()
+
+    for col in df.columns:
+        df[col] = pd.to_numeric(df[col], errors = "coerce")
 
     return df
-
 
