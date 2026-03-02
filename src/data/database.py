2d4e0c441200c99cfdadf370b7b9d76d5a8796e8
jctaylor11 <68626010+jctaylor11@users.noreply.github.com>
2026-03-02 21:48:10 +0900

    refactor: extract logic for populating candles into function in database.py
    - Improves readability and maintenance by creating a modular pipeline structure in populate_candles.py

--- src/data/database.py
+++ src/data/database.py
@@ -1,6 +1,10 @@
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
+
+from src.data.historical_data import fetch_binance_ohlcv_all, clean_ohlcv_data
+
 
 def get_engine():
     load_dotenv()
@@ -18,3 +22,35 @@ def get_engine():
         raise ConnectionError(f"Failed to connect to database: {e}")
         
     return engine
+
+
+def populate_candles(pair, interval, start, end, engine):
+    # Fetch and clean data from Binance
+    candles_df_raw = fetch_binance_ohlcv_all(pair, interval, start, end)
+    candles_df = clean_ohlcv_data(candles_df_raw)
+
+    # Map df columns to sql column names 
+    candles_df.reset_index(inplace=True)
+    candles_df.rename(columns={
+        "date": "open_timestamp",
+        "open": "open_price",
+        }, inplace=True)
+
+    with engine.begin() as conn: 
+        # Get or create interval id in lookup table
+        interval_id = conn.execute(text("SELECT id FROM interval_lookup WHERE interval_name = :interval;"), {"interval": interval}).scalar()
+        if interval_id is None: 
+            interval_id = conn.execute(text("INSERT INTO interval_lookup (interval_name) VALUES (:interval) RETURNING id"), {"interval": interval}).scalar()
+
+        # Get or create pair id in lookup table
+        pair_id = conn.execute(text("SELECT id FROM pair_lookup WHERE coin_pair = :symbol;"), {"symbol": pair}).scalar()
+        if pair_id is None: 
+            pair_id = conn.execute(text("INSERT INTO pair_lookup (coin_pair) VALUES (:symbol) RETURNING id"), {"symbol": pair}).scalar()
+
+        # Add pair id and interval id foreign keys to candles df
+        candles_df["pair_id"] = pair_id
+        candles_df["interval_id"] = interval_id
+
+        # Insert candles df to postgres
+        candles_df.to_sql('candles', conn, if_exists='append', index=False)
+
