73608ee98893b8abe768bb40500b42d0712b15cf
jctaylor11 <68626010+jctaylor11@users.noreply.github.com>
2026-03-15 09:18:48 +0900

    feat: add heatmap for single and format axes to %

--- scripts/conditional_outcomes_contingency.py
+++ scripts/conditional_outcomes_contingency.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.ticker import PercentFormatter
 import seaborn as sns
 from sqlalchemy import text
 
@@ -60,6 +61,22 @@ df_raw = pd.read_sql(text(query), conn, params={"number_of_bins": number_of_bins
 
 df_raw['probability'] = df_raw['x_hit_before_y'] / df_raw['bin_candle_count']
 
+## Single plot view
+fig, ax = plt.subplots(figsize=(12,8))
+
+indicator_id = 1
+percentile_bin = 5
+df_bin = df_raw.loc[(df_raw['indicator_id'] == indicator_id) & (df_raw['percentile_bin'] == percentile_bin)]   # Filter for bin
+df_contingency = df_bin.pivot_table(values='probability', index='threshold_x', columns='threshold_y')
+
+sns.heatmap(df_contingency, ax=ax, cmap='viridis')
+fig.suptitle(f"Probability of X before Y, Percentile Bin {percentile_bin}", fontsize=24)
+ax.set_xticklabels([f"{x*100:.0f}%" for x in df_contingency.columns])
+ax.set_yticklabels([f"{y*100:.0f}%" for y in df_contingency.index])
+ax.invert_yaxis()
+plt.show()
+
+## All bin subplots view
 fig, axs = plt.subplots(3,4,figsize=(16, 9))
 axs = axs.flatten()     # To make it iterable
 
@@ -70,26 +87,27 @@ p_max = df_raw['probability'].max()
 # Iterates through each bin and adds as subplot
 total_bins = df_raw['percentile_bin'].unique()
 for percentile_bin in total_bins:
-    df_bin = df_raw.loc[(df_raw['indicator_id'] == 1) & (df_raw['percentile_bin'] == percentile_bin)]   # Filter for bin
+    df_bin = df_raw.loc[(df_raw['indicator_id'] == 1) & (df_raw['percentile_bin'] == percentile_bin)] 
     df_contingency = df_bin.pivot_table(values='probability', index='threshold_x', columns='threshold_y')
     sns.heatmap(df_contingency, ax=axs[percentile_bin-1], cmap='viridis', vmin=p_min, vmax=p_max, cbar=False)
     axs[percentile_bin-1].invert_yaxis()
     axs[percentile_bin-1].set_title(f"Percentile Bin {percentile_bin}")
 
 # Plot and colour bar spacings
-fig.subplots_adjust(left=0.05, right=0.85, top=0.92, bottom=0.12, hspace=0.6, wspace=0.4)
+fig.subplots_adjust(left=0.05, right=0.85, top=0.92, bottom=0.12, hspace=0.6, wspace=0.4)   
 cbar_ax = fig.add_axes([0.9, 0.12, 0.02, 0.78])  # [left, bottom, width, height]
 
 # One colour bar normalised by the global min and max probabilities
 fig.colorbar(plt.cm.ScalarMappable(norm=plt.Normalize(p_min, p_max), cmap='viridis'), cax=cbar_ax, label='Probability')
-fig.suptitle("Percentile Bin X Before Y Heatmap")
+fig.suptitle("Probability X Before Y, All Percentile Bins")
 plt.show()
 
-# All heatmaps on one
+## All heatmaps on one
 df_multi_contingency = df_raw.pivot_table(values='probability', index='threshold_x', columns=['percentile_bin', 'threshold_y'])   # Multi column index
 
 fig, axs = plt.subplots(figsize=(16, 9))
 sns.heatmap(df_multi_contingency, cmap='viridis')
 axs.invert_yaxis()
-fig.suptitle("All Percentile Bins X before Y")
+fig.suptitle("Probability X Before Y, All Percentile Bins Multi-Index")
 plt.show()
+
