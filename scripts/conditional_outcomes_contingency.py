6697e165baf365039a63ab2ee124307e8bee6545
jctaylor11 <68626010+jctaylor11@users.noreply.github.com>
2026-03-18 15:28:56 +0900

    feat: add EV subplot visualisations for all percentile bins

--- scripts/conditional_outcomes_contingency.py
+++ scripts/conditional_outcomes_contingency.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.ticker import PercentFormatter
 import seaborn as sns
 from sqlalchemy import text
 
@@ -84,14 +83,17 @@ axs = axs.flatten()     # To make it iterable
 p_min = df_raw['probability'].min()
 p_max = df_raw['probability'].max()
 
+# Filter for indicator
+df_raw = df_raw[df_raw['indicator_id'] == indicator_id]
+
 # Iterates through each bin and adds as subplot
 total_bins = df_raw['percentile_bin'].unique()
-for percentile_bin in total_bins:
-    df_bin = df_raw.loc[(df_raw['indicator_id'] == 1) & (df_raw['percentile_bin'] == percentile_bin)] 
+for i, p_bin in enumerate(sorted(total_bins)):               # TODO: should be filtered for indicator id outside of the loop 
+    df_bin = df_raw.loc[(df_raw['percentile_bin'] == p_bin)] 
     df_contingency = df_bin.pivot_table(values='probability', index='threshold_a', columns='threshold_b')
-    sns.heatmap(df_contingency, ax=axs[percentile_bin-1], cmap='viridis', vmin=p_min, vmax=p_max, cbar=False)
-    axs[percentile_bin-1].invert_yaxis()
-    axs[percentile_bin-1].set_title(f"Percentile Bin {percentile_bin}")
+    sns.heatmap(df_contingency, ax=axs[i], cmap='viridis', vmin=p_min, vmax=p_max, cbar=False)
+    axs[i].invert_yaxis()
+    axs[i].set_title(f"Percentile Bin {p_bin}")
 
 # Plot and colour bar spacings
 fig.subplots_adjust(left=0.06, right=0.89, top=0.85, bottom=0.11, hspace=0.6, wspace=0.4)   
@@ -108,19 +110,17 @@ df_multi_contingency = df_raw.pivot_table(values='probability', index='threshold
 fig, axs = plt.subplots(figsize=(16, 9))
 sns.heatmap(df_multi_contingency, cmap='viridis')
 axs.invert_yaxis()
-fig.suptitle(f"Probability of Threshold A Before Treshold B\nIndicator ID {indicator_id}, All Percentile Bins Multi-Index", fontsize=24)
+fig.suptitle(f"Probability of Threshold A Before Threshold B\nIndicator ID {indicator_id}, All Percentile Bins Multi-Index", fontsize=24)
 plt.show()
 
-
-## EV Heatmap, fixed ID and bin with fixed ID and percentile bin
-df_ev_raw = df_raw.loc[(df_raw['indicator_id'] == indicator_id) & (df_raw['percentile_bin'] == percentile_bin)]   # Filter for bin
-df_ev_slim = df_ev_raw[['threshold_a', 'threshold_b', 'probability']]
-
+## Single EV plot
 # Self join to retrieve P(B before A) by flipping the index on P(A before B)
-df_ev = df_ev_slim.merge(df_ev_slim, left_on=['threshold_a', 'threshold_b'], right_on=['threshold_b', 'threshold_a'], suffixes=['_A', '_B'])
-df_ev['ev'] = abs(df_ev['threshold_a_A']) * df_ev['probability_A'] + (-abs(df_ev['threshold_b_A']) * df_ev['probability_B'])
+df_ev_merged = df_raw.merge(df_raw, left_on=['threshold_a', 'threshold_b'], right_on=['threshold_b', 'threshold_a'], suffixes=['_A', '_B'])
+df_ev_merged['ev'] = abs(df_ev_merged['threshold_a_A']) * df_ev_merged['probability_A'] + (-abs(df_ev_merged['threshold_b_A']) * df_ev_merged['probability_B'])
 
-ev_contingency = df_ev.pivot_table(values='ev', index='threshold_a_A', columns='threshold_b_A')
+# Filter for bin and indicator to display
+df_ev_bin = df_ev_merged.loc[(df_ev_merged['indicator_id_A'] == indicator_id) & (df_ev_merged['percentile_bin_A'] == percentile_bin)].copy()   # Filter for bin
+ev_contingency = df_ev_bin.pivot_table(values='ev', index='threshold_a_A', columns='threshold_b_A')
 
 # Plot heatmap
 fig, ax = plt.subplots(figsize=(12,8))
@@ -128,5 +128,38 @@ sns.heatmap(ev_contingency, cmap='RdBu', center=0)
 ax.invert_yaxis()
 ax.set_ylabel("Take Profit")
 ax.set_xlabel("Stop Loss")
-fig.suptitle(f"Expected Value for Take-Profit/Stop-Loss Setup\nFor Inidicator ID {indicator_id} in Percentile Bin {percentile_bin}", fontsize=24)
+fig.suptitle(f"Expected Value for Take-Profit/Stop-Loss Setup\nFor Indicator ID {indicator_id} in Percentile Bin {percentile_bin}", fontsize=24)
+plt.show()
+
+## Subplots
+fig, axs = plt.subplots(3,4,figsize=(16, 9))
+axs = axs.flatten()     # To make it iterable
+
+# Filter for indicator
+df_ev_merged = df_ev_merged.loc[df_ev_merged['indicator_id_A'] == indicator_id].copy()
+
+# Get extreme values for colour bar
+ev_max = df_ev_merged['ev'].max()
+ev_min = df_ev_merged['ev'].min()
+
+# Loop through each bin and add to the plot
+total_bins = df_ev_merged['percentile_bin_A'].unique()
+for i, p_bin in enumerate(sorted(total_bins)): 
+    df_ev_bin = df_ev_merged.loc[df_ev_merged['percentile_bin_A'] == p_bin]
+    ev_contingency = df_ev_bin.pivot_table(values='ev', index='threshold_a_A', columns='threshold_b_A')
+    sns.heatmap(ev_contingency, ax=axs[i], cmap='RdBu', vmin=ev_min, vmax=ev_max, cbar=False)
+
+    axs[i].invert_yaxis()
+    axs[i].set_title(f"Percentile Bin {p_bin}")
+
+    axs[i].set_ylabel("Take Profit")
+    axs[i].set_xlabel("Stop Loss")
+
+# Plot and colour bar spacings
+fig.subplots_adjust(left=0.06, right=0.89, top=0.85, bottom=0.11, hspace=0.6, wspace=0.4)   
+cbar_ax = fig.add_axes([0.93, 0.12, 0.02, 0.78])  # [left, bottom, width, height]
+
+# One colour bar normalised by the global min and max probabilities
+fig.colorbar(plt.cm.ScalarMappable(norm=plt.Normalize(ev_min, ev_max), cmap='RdBu'), cax=cbar_ax, label='EV')
+fig.suptitle(f"Expected Value for Take-Profit/Stop-Loss Setup\nIndicator ID {indicator_id}, All Percentile Bins", fontsize=24)
 plt.show()
